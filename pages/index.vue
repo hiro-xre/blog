2a8d86fe0ffe812adc5fd6bf3fd47c05aa00e23b
hiroto-hara <xre0605@gmail.com>
2025-01-07 21:27:05 +0900

    refactor:TOPページの掃除

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,11 @@
 <template>
   <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme>
-            <USelect
-              v-model="$colorMode.preference"
-              :options="['system', 'light', 'dark']"
-            />
-          </ColorScheme>
-        </div>
-      </template>
-      <UButton
-        icon="i-heroicons-book-open"
-        to="https://ui.nuxt.com"
-        target="_blank"
-      >
-        Open Nuxt UI Documentation
-      </UButton>
-    </UCard>
+    <ColorScheme>
+      <USelect
+        v-model="$colorMode.preference"
+        :options="['system', 'light', 'dark']"
+      />
+    </ColorScheme>
     <NuxtLink :to="{ name: 'blogs' }">
       Blog一覧へ
     </NuxtLink>
