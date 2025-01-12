321d6ac026cedde84ce288ec054c119fafcb09e7
hiroto-hara <xre0605@gmail.com>
2025-01-12 22:16:34 +0900

    feat:mdcを導入する

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/content'],
   devtools: { enabled: true },
   css: ['~/assets/stylesheets/globals.css'],
+  content: {},
   eslint: {
     config: {
       stylistic: {
