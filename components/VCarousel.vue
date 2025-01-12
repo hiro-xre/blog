8d526ba0e933382f441e9452b87d15dbd13698a0
hiroto-hara <xre0605@gmail.com>
2025-01-12 11:23:16 +0900

    feat:Carouselコンポーネントを実装する

---
+++ components/VCarousel.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+type Props = {
+  items: string[];
+  autoPlay: boolean;
+  autoPlayMS: number;
+  indicators: boolean;
+  width: number;
+  height: number;
+  draggable: boolean;
+};
+const { items = [], autoPlay = false, autoPlayMS = 5000, indicators = false, width = 300, height = 400, draggable = false } = defineProps<Props>();
+
+const carouselRef = ref();
+
+onMounted(() => {
+  if (!autoPlay) return;
+  setInterval(() => {
+    if (!carouselRef.value) return;
+
+    if (carouselRef.value.page === carouselRef.value.pages) {
+      return carouselRef.value.select(0);
+    }
+
+    carouselRef.value.next();
+  }, autoPlayMS);
+});
+</script>
+
+<template>
+  <UCarousel
+    ref="carouselRef"
+    v-slot="{ item }"
+    :items="items"
+    :indicators="indicators"
+  >
+    <img
+      :src="item"
+      :width="width"
+      :height="height"
+      :draggable="draggable"
+    >
+  </UCarousel>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const items: string[] = [
+  '/images/pola_1.jpg',
+  '/images/pola_2.jpg',
+  '/images/art_1.jpg',
+  '/images/art_2.jpg',
+  '/images/art_3.jpg',
+  '/images/beer_1.jpg',
+  '/images/beer_2.jpg',
+];
+</script>
+
 <template>
   <UContainer>
     <ColorScheme>
@@ -9,5 +21,12 @@
     <NuxtLink :to="{ name: 'blogs' }">
       Blog一覧へ
     </NuxtLink>
+    <VCarousel
+      :items="items"
+      auto-play
+      indicators
+      :width="400"
+      :height="600"
+    />
   </UContainer>
 </template>
