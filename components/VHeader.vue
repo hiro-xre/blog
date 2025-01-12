4f71b099280a29840385b596ee982efb7788d15a
hiroto-hara <xre0605@gmail.com>
2025-01-12 22:59:59 +0900

    feat:ダークモード・ライトモード機能を実装する

--- components/VHeader.vue
+++ components/VHeader.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const colorMode = useColorMode('dark');
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark';
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+  },
+});
+</script>
+
 <template>
   <header>
     <nav class="grid h-24 grid-cols-3 gap-2 p-4">
@@ -36,6 +48,14 @@
           icon="i-emojione-monotone-clinking-beer-mugs"
           :to="{ name: 'blogs' }"
         />
+        <ClientOnly>
+          <UButton
+            :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+            color="orange"
+            variant="ghost"
+            @click="isDark = !isDark"
+          />
+        </ClientOnly>
       </div>
     </nav>
   </header>
