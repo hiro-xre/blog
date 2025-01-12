064c426b94d94334e179cf500a345937e31cb6c6
hiroto-hara <xre0605@gmail.com>
2025-01-12 21:14:41 +0900

    feat:今年の目標を執筆

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -2,8 +2,8 @@
   <div>
     <div>Blogs</div>
     <li>
-      <NuxtLink :to="{ name: 'blogs-id', params: { id: 'sample' } }">
-        sample
+      <NuxtLink :to="{ name: 'blogs-id', params: { id: '2025-target' } }">
+        2025年の目標
       </NuxtLink>
     </li>
   </div>
