149c2ac4e3473c9484ca7a2f74ca19d2ed93b50c
hiroto-hara <xre0605@gmail.com>
2025-01-13 14:02:00 +0900

    feat:OGP・アイコンを設定する

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,21 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/content'],
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja', prefix: 'og: https://ogp.me/ns#',
+      },
+      meta: [
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: 'hirolog' },
+        { property: 'og:image', content: '/images/ogp.jpg' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg', href: '/icon.svg' },
+      ],
+    },
+  },
   css: ['~/assets/stylesheets/globals.css'],
   content: {},
   routeRules: {
