f3ed7dc037caea12f99a5b47a573d94c6a8f3e43
hiroto-hara <xre0605@gmail.com>
2025-01-18 15:17:46 +0900

    feat:Nuxt Contentをv3にアップデート tsconfigの設定忘れも合わせて対応しています

--- pages/blogs/[id].vue
+++ pages/blogs/[id].vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
 const route = useRoute();
-const { data } = await useAsyncData(route.params.id, () => queryContent('blogs', route.params.id).findOne());
+const { data } = await useAsyncData(route.path, () => {
+  return queryCollection('content').path(route.path).first();
+});
 
 useHead({
   title: `${route.params.id}`,
   meta: [
-    { property: 'og:description', content: data.value.description },
+    { property: 'og:description', content: data.value?.description },
   ],
 });
 </script>
 
 <template>
   <main>
-    <ContentDoc />
+    <ContentRenderer
+      v-if="data"
+      :value="data"
+    />
   </main>
 </template>
