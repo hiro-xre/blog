e108fb342012ea32b392d673fffb840d6e99714d
hiroto-hara <xre0605@gmail.com>
2025-01-19 16:34:19 +0900

    feat:Listコンポーネントを実装する

---
+++ components/content/VList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type HTMLElementTagName = keyof HTMLElementTagNameMap;
+
+type Props = {
+  tag: Extract<HTMLElementTagName, 'ol' | 'ul'>;
+  items: string[];
+  listStyle: string;
+};
+defineProps<Props>();
+</script>
+
+<template>
+  <slot />
+  <component
+    :is="tag"
+    :class="['list-inside', listStyle]"
+  >
+    <li
+      v-for="(item, index) in items"
+      :key="`${index}:${item}`"
+    >
+      {{ item }}
+    </li>
+  </component>
+</template>
