b7c4846a15c142f97c6df49ced0b004b9fb33d90
hiroto-hara <xre0605@gmail.com>
2025-01-19 17:53:30 +0900

    feat:タイトル・説明でセットのコンポーネントを実装する

---
+++ components/content/VTitleDescription.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+type Props = {
+  title: string;
+  titleSize: string;
+};
+defineProps<Props>();
+</script>
+
+<template>
+  <div
+    :class="titleSize"
+  >
+    {{ title }}
+  </div>
+  <slot />
+</template>
